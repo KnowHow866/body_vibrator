4e20d2bf63005dfee2c6c2738e3535a5a6318842
KnowHow866 <accidental001@gmail.com>
2018-11-18 20:27:42 +0800

    feat(set device port as --device_port param)

--- gui.py
+++ gui.py
@@ -5,15 +5,11 @@ import time
 # global variable to sent and set
 amplitude = 0
 
-<<<<<<< HEAD
-# arduino_serial = serial.Serial(device_port, 9600, timeout=.1)
-=======
 # tkinter setting
 tkBoard = Tk()
 tkBoard.minsize(width=320, height=170)
 tkBoard.config(bg='yellow')
 tkBoard.title("GUI for Arduino")
->>>>>>> 634995f32a9d4130bb7593154e9be912bf93c906
 
 # arduino serial setting
 device_port = '/dev/cu.usbserial-AI0443U0'
@@ -22,25 +18,25 @@ value = arduino_serial.readline()
 
 arduino_serial.write(b'1')
 read_block = arduino_serial.readline()
-if read_block:
-    print(read_block)
 
-"""
-while True:
-    for i in range(4): # for the 1st 2 seconds
-        arduino_serial.write( str.encode( str(i + 1) ) )
-        read_block = arduino_serial.readline()
-        if read_block:
-            print(read_block)
-        time.sleep(0.5)
-        """
-while True:
-    for i in range(4):
-        tmp = ['0', '0', '0', '0']
-        tmp[i] = '1';
-        str_to_send = "".join(tmp)
-        print(str_to_send)
-        arduino_serial.write(str.encode(str_to_send))
-        if read_block:
-            print(read_block)
-        time.sleep(0.5)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=
+		'''
+		Program to run GUI interface to control arduino device body_vibrator
+		'''
+	)
+    parser.add_argument('--device_port', help='path to USB serial port of arduino, usually under /dev of unix like system')
+    args = parser.parse_args()
+    
+    if args.device_port: device_port = args.device_port
+
+    while True:
+        for i in range(4):
+            tmp = ['0', '0', '0', '0']
+            tmp[i] = '1';
+            str_to_send = "".join(tmp)
+            print(str_to_send)
+            arduino_serial.write(str.encode(str_to_send))
+            if read_block:
+                print(read_block)
+            time.sleep(0.5)
