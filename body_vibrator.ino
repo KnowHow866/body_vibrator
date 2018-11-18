79b2a4f20b2dd83cbc4db5a77aaa1f42101cdd71
KnowHow866 <accidental001@gmail.com>
2018-11-18 19:47:22 +0800

    merge some files

--- body_vibrator.ino
+++ body_vibrator.ino
@@ -1,16 +1,34 @@
+int Vib[4] = {12, 11, 10, 9};
+int mode = 0; // 0: non-directional, 1: directional
+int amplitude = 1;
 void setup() {
-  Serial.begin (9600);
-  
-  // if use analogWrite to control vibrator, analog Pin do not need to set
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(Vib[0], OUTPUT);
+  pinMode(Vib[1], OUTPUT);
+  pinMode(Vib[2], OUTPUT);
+  pinMode(Vib[3], OUTPUT);
 }
 
 void loop() {
-  Serial.println("Test analog toggle");
-  for(int i = 0; i < 4; ++i) {
-    Serial.print("Toggle with light: ");
-    Serial.println(i*84);
-    
-    analogWrite(A0, i*84);
-    delay(1500) ;
+  // put your main code here, to run repeatedly:
+  while (Serial.available()) {
+    if (mode) { // mode: 1
+  
+    }
+    else { // mode: 0
+      amplitude = Serial.read() - '0';
+      Serial.print("I received amplitude: " + amplitude);
+      for (int i  = 0; i < 4; i ++) {
+        if (i < amplitude) {
+          digitalWrite(Vib[i], HIGH);
+        }
+        else {
+          digitalWrite(Vib[i], LOW);  
+        }
+      }
+      delay(500);
+    }
   }
 }
