c22491b40f0f343e79b3dee46860e5af931288be
yu-zhen <wlphg010688@gmail.com>
2018-11-18 20:19:59 +0800

    the one that has bug

--- gui.py
+++ gui.py
@@ -1,28 +1,42 @@
-
-# test communicate with arduino by serial bus
-# reference doc: https://playground.arduino.cc/interfacing/python
-
-import argparse
 import serial
-import tkinter as tk
+from tkinter import *
+import time
 
-device_port = '/dev/cu.usbserial-AI0443U0' # default to holis macBook, please set as your own port
+# global variable to sent and set
+amplitude = 0
 
-arduino_serial = serial.Serial(device_port, 9600, timeout=.1)
+# tkinter setting
+tkBoard = Tk()
+tkBoard.minsize(width=320, height=170)
+tkBoard.config(bg='yellow')
+tkBoard.title("GUI for Arduino")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=
-		'''
-		Program to run GUI interface to control arduino device body_vibrator
-		'''
-	)
-    parser.add_argument('--device_port', help='path to USB serial port of arduino, usually under /dev of unix like system')
-    args = parser.parse_args()
+# arduino serial setting
+device_port = '/dev/cu.usbserial-AI0443U0'
+arduino_serial = serial.Serial(device_port, 9600, timeout=1)
+value = arduino_serial.readline()
 
-    if args.device_port: arduino_serial = args.device_port
+arduino_serial.write(b'1')
+read_block = arduino_serial.readline()
+if read_block:
+    print(read_block)
 
-    print('Serial read test'.ljust(100, '.'))
-    while True:
+"""
+while True:
+    for i in range(4): # for the 1st 2 seconds
+        arduino_serial.write( str.encode( str(i + 1) ) )
         read_block = arduino_serial.readline()
         if read_block:
             print(read_block)
+        time.sleep(0.5)
+        """
+while True:
+    for i in range(4):
+        tmp = ['0', '0', '0', '0']
+        tmp[i] = '1';
+        str_to_send = "".join(tmp)
+        print(str_to_send)
+        arduino_serial.write(str.encode(str_to_send))
+        if read_block:
+            print(read_block)
+        time.sleep(0.5)
